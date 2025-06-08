d0b345ef920ab19954b47ec26896cc27e063c58c
SunnexGB <sibircevtt@gmail.com>
2025-06-08 14:53:18 +0300

    finished second part code(steam.js)

--- configuration/config.js
+++ configuration/config.js
@@ -1,6 +1,7 @@
 const defurl = 'https://api.brawlhalla.fr/angularwebapp2/';
 const patch = 'https://api.brawlhalla.fr/angularwebapp2/getPatch'; 
 const steamapiurl = "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=";
+const steamlink = "https://api.brawlhalla.fr/angularwebapp2/playerMainSteam?name="
 module.exports = {
     defurl,
     patch,
--- src/steam.js
+++ src/steam.js
@@ -4,15 +4,13 @@ const { steamapiurl } = require('../configuration/config');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../configuration/.env') });
 const readline = require('readline');
-
 const askapisteam = () => {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-
   return new Promise((resolve) => {
-    rl.question('Please enter your steam API key: ', (apiKey) => {
+    rl.question('Please enter your Steam API key: ', (apiKey) => {
       rl.question('Do you want to save this API key in your .env file? (y/n): ', (answer) => {
         if (answer.toLowerCase() === 'y') {
           fs.appendFileSync('.env', `\nSTEAM_API_KEY=${apiKey}`, 'utf8');
@@ -29,7 +27,6 @@ const Sreamidm = async (vanity) => {
   if (!apiKey) {
     apiKey = await askapisteam();
   }
-
   try {
     const url = `${steamapiurl}${apiKey}&vanityurl=${vanity}`;
     const response = await axios.get(url);
@@ -40,9 +37,19 @@ const Sreamidm = async (vanity) => {
     } else {
       throw new Error('User not found');
     }
-  }
-    catch (error) {
+  } catch (error) {
     throw error;
   }
 };
-module.exports = Sreamidm;
+const steambhdata = async (steamId) => {
+  try {
+    const brawlhallaUrl = `https://api.brawlhalla.fr/angularwebapp2/playerMainSteam?name=${steamId}`;
+    const response = await axios.get(brawlhallaUrl);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching Brawlhalla data: ' + error.message);
+  }
+};
+module.exports = { Sreamidm, steambhdata };
+
+//Forget it, i wrote first part code and then went to drink tea and forgot about other part code. It will be like a hint for curious people.
